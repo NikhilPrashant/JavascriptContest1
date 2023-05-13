61997edce18fff82bc8a74ee595a54c9fc7b269e
nikhilprashant <nikhilprashant1998@gmail.com>
2023-05-13 16:12:44 +0530

    Correct Output

--- script.js
+++ script.js
@@ -8,20 +8,40 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  const studentsWithMarksOver50 = arr.filter(arr => arr.marks > 50);
+  studentsWithMarksOver50.map(arr => console.log(arr));
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(arr => {
+    if (arr.marks > 50) {
+      console.log(arr);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  const newStudent = { id: 4, name: "susan", age: "20", marks: 45 };
+  arr.push(newStudent);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  const passedStudents = arr.filter(e => e.marks >= 50);
+  arr.length = 0;
+  passedStudents.forEach(e => arr.push(e));
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  const newStudents = [
+    { id: 5, name: "alice", age: "21", marks: 75 },
+    { id: 6, name: "bob", age: "22", marks: 90 },
+    { id: 7, name: "emma", age: "19", marks: 60 }
+  ];
+  arr.push(...newStudents);
+  console.log(arr);
 }
